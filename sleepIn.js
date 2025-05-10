b8dc88088d4c0061adc2a7ef027dc93de6ef8dc4
jasur737 <baxtiyorovjasur728@gmail.com>
2025-05-10 21:34:03 +0500

    ;)

--- sleepIn.js
+++ sleepIn.js
@@ -7,3 +7,55 @@ function sleepIn(weekday, vacation){
     return true
   }
 }
+
+
+function monkeyTrouble(aSmile, bSmile) {
+  if (aSmile == bSmile) {
+    return true;
+  } else if (aSmile > bSmile) {
+    return false;
+  } else {
+    return false;
+  }
+}  
+
+
+
+function sumDouble(a, b){
+  if (a == b) {
+    return (a + b) * 2
+  } else {
+    return a + b
+  }
+ }
+
+
+ function diff21(n){
+  if (n > 21) {
+    return (n - 21) * 2
+  } else {
+    return 21 - n
+  }
+}
+
+
+
+function parrotTrouble(talking, hour){
+  if (talking & (hour < 7 || hour > 20)) {
+    return true
+  } else {
+    return false
+  }
+  
+}
+
+
+function makes10(a, b){
+  if (a == 10 || b == 10) {
+    return true
+  } else if (a + b == 10) {
+    return true
+  } else {
+    return false
+  }
+}
